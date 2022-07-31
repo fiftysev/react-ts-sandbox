b83e2d4940e4b5bff7f3fd8082e6ff63faee53b4
fiftysev <nesgorai@bk.ru>
2022-07-31 19:56:15 +1000

    Add MUI + RTKQ

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { createTheme, CssBaseline, ThemeProvider, Typography } from '@mui/material';
 import React from 'react';
 
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
 function App() {
-  return <h1>App</h1>;
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Typography variant='h3' textAlign='center' fontWeight='bold'>
+        Example app
+      </Typography>
+    </ThemeProvider>
+  );
 }
 
 export default App;
